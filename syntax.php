dc9abebe368bf1464ab107ae7344da7bff416112
Andreas Gohr <andi@splitbrain.org>
2016-02-02 13:07:01 +0100

    Adjust method signatures to match parent

--- syntax.php
+++ syntax.php
@@ -59,7 +59,7 @@ class syntax_plugin_dwcommits extends DokuWiki_Syntax_Plugin {
     /**
      * Handle the match
      */
-    function handle($match, $state, $pos, &$handler){
+    function handle($match, $state, $pos, Doku_Handler $handler){
         switch ($state) {
           case DOKU_LEXER_ENTER :
                 return array($state, "");
@@ -84,7 +84,7 @@ class syntax_plugin_dwcommits extends DokuWiki_Syntax_Plugin {
     /**
      * Create output
      */
-    function render($mode, &$renderer, $data) {
+    function render($mode, Doku_Renderer $renderer, $data) {
         if($mode == 'xhtml'){
             $this->expire_cache();
             list($state,$match) = $data;
